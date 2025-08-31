4397ba71f6deb351ddac952a6283cff5557cffd2
IHaZeM99 <hazem.a9021@gmail.com>
2025-08-31 20:37:44 +0300

    Fix GitHub Pages deployment configuration

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,6 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react() , tailwindcss(),],
+  base: '/elevvo_task_8/',  // This should match your repository name
+  plugins: [react(), tailwindcss()],
 })
